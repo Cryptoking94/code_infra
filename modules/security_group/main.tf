ce3d73a4eb3949480ee6518670934ca5d3297954
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2024-08-10 22:47:49 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "testing_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["49.206.135.36/32"]
   }
 
   ingress {
