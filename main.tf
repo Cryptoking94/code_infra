344f68e2b2f1840cf07897dac0c96b760d0ec485
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2024-08-10 22:02:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0, < 5.0"
+    }
+  }
+
+  required_version = ">= 0.13"
+}
 provider "aws" {
   region = "us-east-1" # Update with your desired AWS region
   # Add other authentication parameters if necessary
