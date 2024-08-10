6e82989e8e9654bcae486a29beb94e95230c25d2
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2024-08-10 21:31:05 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,21 +1,6 @@
 resource "aws_instance" "testing_instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
-
-  provisioner "remote-exec" {
-    inline = [
-	"curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash",
-	 ". ~/.nvm/nvm.sh",
-	 "nvm install v17.0.0",
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("/home/ubuntu/Testing-Enviorment/ssh-key.pem") # Change the path to your private key file
-      host        = self.public_ip
-    }
-  }
   associate_public_ip_address = true
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids  # Use vpc_security_group_ids instead of security_group_ids
